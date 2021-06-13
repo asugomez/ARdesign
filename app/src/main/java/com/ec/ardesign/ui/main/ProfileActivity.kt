894edccbf321bed076a67cda3e6deed4ba088671
Lucie <fontaluc@gmail.com>
2021-06-13 10:58:52 +0200

    PreferenceActivity -> AppCompatActivity

--- app/src/main/java/com/ec/ardesign/ui/main/ProfileActivity.kt
+++ app/src/main/java/com/ec/ardesign/ui/main/ProfileActivity.kt
@@ -1,15 +1,13 @@
 package com.ec.ardesign.ui.main
 
 import android.os.Bundle
-import android.preference.PreferenceActivity
 import android.support.v7.app.AppCompatActivity
 import com.ec.ardesign.R
 
-class SettingsActivity : PreferenceActivity() {
+class ProfileActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.preferences)
-
-
+        setContentView(R.layout.activity_profile)
     }
 }
