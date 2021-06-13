91e50606c56db7447b4107668aff6c4fd4b5fe88
Bowen <bowenwu2000@yahoo.fr>
2021-06-13 18:45:02 +0200

    Accueil Layout

---
+++ app/src/main/java/com/ec/ardesign/ui/main/AccueilActivity.kt
@@ -0,0 +1,54 @@
+package com.ec.ardesign.ui.main
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import com.ec.ardesign.R
+
+
+class AccueilActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.accueil)
+
+        // bouton Mesure
+        val btnMesure = findViewById<Button>(R.id.mesure_btn)
+        btnMesure.setOnClickListener(this)
+
+        // bouton Visualisation
+        val btnVisualisation = findViewById<Button>(R.id.mesure_btn)
+        btnMesure.setOnClickListener(this)
+
+    }
+
+    /**
+     * Gestion des boutons
+     */
+    override fun onClick(v: View?) {
+
+        /*
+        when (v!!.id) {
+            R.id.mesure_btn ->                  // vers mesure activity
+                val intent = Intent(this, MesureActivity::class.java)
+                // .apply { putExtra(EXTRA_MESSAGE, "msg")}
+                startActivity(intent)
+            R.id.visualisation_btn ->           // vers visualisation activity
+                val intent = Intent(this, VisualisationActivity::class.java)
+                // .apply { putExtra(EXTRA_MESSAGE, "msg")}
+                startActivity(intent)
+        }
+
+         */
+
+        when (v!!.id) {
+            R.id.mesure_btn -> startActivity(Intent(this, MesureActivity::class.java))
+            R.id.visualisation_btn -> startActivity(Intent(this, VisualisationActivity::class.java))
+        }
+
+    }
+
+}
