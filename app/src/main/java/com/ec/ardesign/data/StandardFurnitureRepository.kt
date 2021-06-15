da0cf00972894da28359cdca6e59cbf771d6607d
asuncion.gomez@ug.uchile.cl <zSeDcASDF@249>
2021-06-15 17:19:49 +0200

    standfurn

--- app/src/main/java/com/ec/ardesign/data/StandardFurnitureRepository.kt
+++ app/src/main/java/com/ec/ardesign/data/StandardFurnitureRepository.kt
@@ -1,6 +1,8 @@
 package com.ec.ardesign.data
 
 import android.app.Application
+import com.ec.ardesign.data.model.Furniture
+import com.ec.ardesign.data.model.StandardFurniture
 import com.ec.ardesign.data.source.local.LocalDataProvider
 import com.ec.ardesign.data.source.remote.RemoteDataProvider
 
@@ -14,6 +16,34 @@ class StandardFurnitureRepository(
                 localDataProvider = LocalDataProvider(application),
                 remoteDataProvider = RemoteDataProvider()
             )
+        }
+    }
+    suspend fun getStandFurnitures(hash: String): List<StandardFurniture>{
+        return try{
+            remoteDataProvider.getStandFurnitures(hash).also {
+                localDataProvider.saveOrUpdateStandFurn(it)
+            }
+        } catch (e:Exception){
+            localDataProvider.getStandFurnitures()
+        }
+    }
+
+
+    suspend fun getStandFurnitureData(idFurn: Int, hash: String): StandardFurniture {
+        return try{
+            remoteDataProvider.getStandFurnitureData(idFurn, hash).also {
+                localDataProvider.saveOrUpdateStandFurn(listOf(it))
+            }
+        } catch (e:Exception){
+            localDataProvider.getStandFurnitureData(idFurn)
+        }
+    }
+
+    suspend fun addUsersFurniture(width: String, height: String, length: String, url: String,hash: String){
+        return try{
+            remoteDataProvider.addStandFurniture(width, height, length, url, hash)
+        } catch (e:Exception){
+            localDataProvider.addStandFurniture(width, height, length, url)
         }
     }
 }
