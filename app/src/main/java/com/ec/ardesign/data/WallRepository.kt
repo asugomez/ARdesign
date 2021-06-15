890acc289734830a38405ee42d74ae44ab4765f1
asuncion.gomez@ug.uchile.cl <zSeDcASDF@249>
2021-06-15 17:04:39 +0200

    wall repository

--- app/src/main/java/com/ec/ardesign/data/WallRepository.kt
+++ app/src/main/java/com/ec/ardesign/data/WallRepository.kt
@@ -29,4 +29,52 @@ class WallRepository(
         }
     }
 
+    suspend fun getWallData(idWall: Int, hash: String): Wall{
+        return try{
+            remoteDataProvider.getWallData(idWall, hash).also {
+                localDataProvider.saveOrUpdateWall(listOf(it))
+            }
+        } catch (e:Exception){
+            localDataProvider.getWallData(idWall)
+        }
+    }
+
+    suspend fun getUsersWalls(id_user: Int, hash: String): List<Wall>{
+        return try{
+            remoteDataProvider.getUsersWalls(id_user, hash).also {
+                localDataProvider.saveOrUpdateWall(it)
+            }
+        } catch (e:Exception){
+            localDataProvider.getUsersWalls(id_user)
+        }
+    }
+
+    suspend fun getUsersWall(id_user: Int, id_wall: Int, hash: String): Wall{
+        return try{
+            remoteDataProvider.getUsersWall(id_user, id_wall, hash).also {
+                localDataProvider.saveOrUpdateWall(listOf(it))
+            }
+        } catch (e:Exception){
+            localDataProvider.getUsersWall(id_user, id_wall)
+        }
+    }
+
+    suspend fun addUsersWall(id_user: Int, width: String, height: String, hash:String){
+        return try{
+            remoteDataProvider.addUsersWall(id_user, width, height, hash)
+        } catch (e:Exception){
+            localDataProvider.addUsersWall(id_user, width, height)
+        }
+    }
+
+    suspend fun rmUsersWall(idUser: Int, idWall: Int,  hash:String){
+        return try{
+            remoteDataProvider.rmUsersWall(idUser, idWall, hash)
+        } catch (e:Exception){
+            localDataProvider.rmUsersWall(idWall, idUser)
+        }
+    }
+
+
+
 }
